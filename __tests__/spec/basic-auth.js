ecae72ee15582de773e7738040c1bc840ca8cf06
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2018-11-21 11:48:12 +0000

    Adds some placeholder specs to identify what needs testing

--- __tests__/spec/basic-auth.js
+++ __tests__/spec/basic-auth.js
@@ -1,17 +1,58 @@
 /* eslint-env jest */
-var request = require('supertest')
-var app = require('../../server.js')
+const request = require('supertest')
+const app = require('../../server.js')
+const misconfiguredMarkup = '<h1>Error:</h1><p>Username or password not set. ' +
+                          '<a href="https://govuk-prototype-kit.herokuapp.com/docs/publishing-on-heroku#6-set-a-username-and-password">' +
+                          'See guidance for setting these</a>.</p>'
 /**
  * Check that basic auth
  */
-describe('The Prototype Kit', () => {
+describe('The Prototype Kit - Basic Auth', () => {
   describe('basic auth', () => {
-    it('should request user name ', async () => {
-      const response = await request(app)
-        .get('/')
-        .auth('the-username', 'the-password')
+    describe('misconfigured server  - no user name or password set', () => {
+      xit('should return 501', async () => {
+        const response = await request(app)
+          .get('/')
+          .auth('the-username', 'the-password')
+        expect(response.statusCode).toBe(302)
+      })
 
-      expect(response.statusCode).toBe(302)
+      it('should return HTML', async () => {
+        const response = await request(app)
+          .get('/')
+          .auth('the-username', 'the-password')
+        expect(response.type).toBe('text/html')
+      })
+
+      it('should return an error message for the user', async () => {
+        const response = await request(app)
+          .get('/')
+          .auth('the-username', 'the-password')
+        expect(response.text).toBe(misconfiguredMarkup)
+      })
+    })
+
+    xdescribe('user provides invalid creditials', () => {
+      xdescribe('incorrect username', () => {
+        it('should return 401', () => {})
+        it('should return a header WWW-Authenticate set', () => {})
+      })
+      xdescribe('incorrect password', () => {
+        it('should return 401', () => {})
+        it('should return a header WWW-Authenticate set', () => {})
+      })
+    })
+
+    xdescribe('user provides valid creditials', () => {
+      it('should return 200', () => {})
+      it('should return HTML', () => {})
+
+      it('should not return an error message to the user', async () => {
+        const response = await request(app)
+          .get('/')
+          .auth('the-username', 'the-password')
+        expect(response.text).not.toBe(misconfiguredMarkup)
+      })
     })
   })
 })
